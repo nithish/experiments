d2271df9691f88d11f2d03162f567075f643e2a2
nithish <nithish@pc>
2018-07-23 19:56:11 +0530

    Retrieved Optional Header address

--- IAT hooking/iat-hooking.c
+++ IAT hooking/iat-hooking.c
@@ -23,39 +23,26 @@ int main()
 {
     PULONG opt = 0;
     BYTE* procMem = malloc(BUFSIZ);
-    pbi = malloc(sizeof(*pbi));
     FARPROC  ntQueryInformationProcess;
     short readSuccess = 0;
+
     IMAGE_IMPORT_DESCRIPTOR *iatDesc = malloc(sizeof(IMAGE_IMPORT_DESCRIPTOR));
-    IMAGE_NT_HEADERS64 *ntHdr = malloc(sizeof(IMAGE_NT_HEADERS64));
-    // DWORD pid = GetProcessId("Taskmgr.exe");
-    // printf("Pid: %d\n",pid);
-    HANDLE proc = OpenProcess(PROCESS_VM_READ,FALSE,GetCurrentProcessId());
-    if(proc == NULL)
-        printf("Error opening process %d\n",GetLastError());
-    /*Run time Loading of Ntdll*/
-    HMODULE hNTDLL = LoadLibrary(TEXT("ntdll.dll"));
-    if(!hNTDLL)
-        return 0;
-    ntQueryInformationProcess = GetProcAddress(hNTDLL,"NtQueryInformationProcess");
-    NTSTATUS status = ntQueryInformationProcess(proc,0,pbi,sizeof(pbi),opt);
-    printf("Address %d\n",pbi->PebBaseAddress);
-    perror("Error");
-    readSuccess = ReadProcessMemory(
-        proc,
-        pbi->PebBaseAddress,
-        procMem,
-        BUFSIZ,
-        0
-    );
-    printf("ReadProcessMemory Error code: %d\n",readSuccess);
-    printf("ProcMem: %d\n",procMem);
-    if(!readSuccess){
-        perror("Failed to read Process memory");
-        printf("Status: %d\n",readSuccess);
-    }
-    ntHdr = (IMAGE_NT_HEADERS64*) procMem;
-    printf("Signature: %x\n",ntHdr->Signature);
-    perror("Error");
-    return 0;
+    IMAGE_FILE_HEADER *ntfHdr = malloc(sizeof(IMAGE_FILE_HEADER)); 
+    IMAGE_NT_HEADERS *ntHdr = malloc(sizeof(IMAGE_NT_HEADERS));
+    IMAGE_DOS_HEADER *dosHdr = malloc(sizeof(IMAGE_DOS_HEADER));
+    IMAGE_OPTIONAL_HEADER *optHdr = malloc(sizeof(IMAGE_OPTIONAL_HEADER));
+    
+    HMODULE mod = GetModuleHandle(0);
+    
+    dosHdr = (IMAGE_DOS_HEADER*) mod;
+    ntHdr = (IMAGE_NT_HEADERS*) ((BYTE*) dosHdr+dosHdr->e_lfanew);
+    ntfHdr = (IMAGE_FILE_HEADER*) &ntHdr->FileHeader;
+    optHdr = (IMAGE_OPTIONAL_HEADER*) &ntHdr->OptionalHeader;
+
+    printf("Dos Signature: %x\n",dosHdr->e_lfanew);
+    printf("Nt Header: %x\n",ntHdr->Signature);
+    printf("File Header: %x\n",ntfHdr->Machine);
+    printf("Optional Header: %x\n",optHdr->Magic);
+
+
 }
