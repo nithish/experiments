1025fc5fc3ccf24e7158d051ec4d8fe3c9847bf0
nithish <nithish@pc>
2018-07-24 20:53:44 +0530

    Retrieved Import DLLs

--- IAT hooking/iat-hooking.c
+++ IAT hooking/iat-hooking.c
@@ -31,18 +31,54 @@ int main()
     IMAGE_NT_HEADERS *ntHdr = malloc(sizeof(IMAGE_NT_HEADERS));
     IMAGE_DOS_HEADER *dosHdr = malloc(sizeof(IMAGE_DOS_HEADER));
     IMAGE_OPTIONAL_HEADER *optHdr = malloc(sizeof(IMAGE_OPTIONAL_HEADER));
-    
+    IMAGE_THUNK_DATA *fThunk = malloc(sizeof(IMAGE_THUNK_DATA));
+    IMAGE_IMPORT_BY_NAME  *impByName,*funcAddr = malloc(sizeof(IMAGE_IMPORT_BY_NAME));
+
     HMODULE mod = GetModuleHandle(0);
-    
+    DWORD va;
     dosHdr = (IMAGE_DOS_HEADER*) mod;
     ntHdr = (IMAGE_NT_HEADERS*) ((BYTE*) dosHdr+dosHdr->e_lfanew);
     ntfHdr = (IMAGE_FILE_HEADER*) &ntHdr->FileHeader;
     optHdr = (IMAGE_OPTIONAL_HEADER*) &ntHdr->OptionalHeader;
-
-    printf("Dos Signature: %x\n",dosHdr->e_lfanew);
+    va =  optHdr->DataDirectory[1].VirtualAddress;
+    DWORD size =  optHdr->DataDirectory[1].Size;
+    DWORD noFiles = size/sizeof(IMAGE_IMPORT_DESCRIPTOR);
+    int a =(int) mod;
+    int b = (int) va;
+    int c =0,d=0,e=0;
+    
+    
+    printf("Base Address: %x\n",mod);
+    printf("Dos Signature: %x\n",dosHdr->e_magic);
     printf("Nt Header: %x\n",ntHdr->Signature);
     printf("File Header: %x\n",ntfHdr->Machine);
     printf("Optional Header: %x\n",optHdr->Magic);
+    
+    while(1){
+        c= a+b;
+        iatDesc = (IMAGE_IMPORT_DESCRIPTOR*) c;
+        d = (int) iatDesc->Name;
+        char* ptr = malloc(sizeof(char*)); 
+        ptr = (char*)(a+d);
+        if(d == 0)
+            break;
+        printf("Imported File: %s\n",ptr);
+        fThunk = (IMAGE_THUNK_DATA*) &iatDesc->FirstThunk;
+        while(1){
+            impByName = (IMAGE_IMPORT_BY_NAME*) &fThunk->u1;
+            funcAddr = (IMAGE_IMPORT_BY_NAME*)(&fThunk->u1.AddressOfData);//check
+            int t = (int) funcAddr->Hint;
+            long* u = (long*) (a+t);
 
+            printf("Hint: %s\n",*u);
+            if(impByName->Hint == 0)
+                break;
+            int x = (int)fThunk;
+            int y = x+sizeof(IMAGE_THUNK_DATA);
+            fThunk = (IMAGE_THUNK_DATA*) y;
+        }
+        b+=sizeof(IMAGE_IMPORT_DESCRIPTOR);
+    }
+    perror("Thunk Error: ");
 
 }
